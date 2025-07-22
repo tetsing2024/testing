82acc94941141e622c3a89825f3d9a8e8c22a7c5
tetsing2024 <testingexaleap@gmail.com>
2025-07-22 14:04:17 +0530

    Update and rename file1.py to test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+# missing_colon.py - This file has a syntax error: missing colon
+
+def division(a: float, b: float) -> float
+    return a / b
+
+if __name__ == "__main__":
+    print(division(10, 5))
